8e64f86e83750c1f65ba9725e03dbe8c1c24b907
Sho Hashimoto <hashimoto@shokai.org>
2015-09-05 21:27:41 +0900

    arduino analog-read sample

---
+++ dump/arduino/arduino.ino
@@ -0,0 +1,23 @@
+// read analog value and report
+
+void setup() {
+  pinMode(13, true);
+  Serial.begin(9600);
+}
+
+int an;
+
+void loop() {
+  an = analogRead(0);
+  Serial.println(an);
+  if(MIDIUSB.available()){
+    MIDIEvent e = {0x0B, 0xB0|1, 10, an>>3};
+    MIDIUSB.write(e);
+  }
+
+  // blink LED
+  digitalWrite(13, true);
+  delay(100);
+  digitalWrite(13, false);
+  delay(100);
+}
